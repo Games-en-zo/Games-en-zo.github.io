d576b9dfeab8e9d81e09bb3e72ece90666ffc725
Games-en-zo <63015838+Games-en-zo@users.noreply.github.com>
2020-04-03 00:00:23 +0100

    Create mobile.js

---
+++ scripts/mobile.js
@@ -0,0 +1,25 @@
+			function IsMobile()
+			{
+				console.log(window.outerWidth);
+				console.log(window.outerHeight);
+				if(window.outerWidth < 760)
+				{	
+					return true;
+				}
+				else
+				{
+					return false;
+				}	
+			}
+				
+			function Mobile()
+			{
+				if(IsMobile())
+				{
+					var linkElement = document.getElementById('unityContainer');
+					if(linkElement != null)
+					{
+						linkElement.parentNode.removeChild(element);
+					}	
+				}
+			}
